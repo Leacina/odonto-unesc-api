128686636ce83276588ce1ec0436d21f244a9ca5
Wilian <wilianx7@hotmail.com>
2019-11-23 15:30:57 -0300

    Add Migration create-script-table

---
+++ database/migrations/20191123181416-create-script-table.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('script', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      shared: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      active: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      teacher: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Teacher',
+          key: 'id'
+        }
+      },
+      cases: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Case',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('script');
+  }
+};
