8cf724b11bd3f8735f6ca96ebc179387b0ac146d
Wilian <wilianx7@hotmail.com>
2019-11-24 15:36:12 -0300

    Add ScriptCaseModel

---
+++ src/models/ScriptCase.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Script_Case = sequelize.define('Script_Case', {
+        id_script: DataTypes.INTEGER,
+        id_case: DataTypes.INTEGER
+    }, {
+        //Adicionado para gerar a tabela no singular
+        freezeTableName: true,
+    });
+
+    return Script_Case;
+}
