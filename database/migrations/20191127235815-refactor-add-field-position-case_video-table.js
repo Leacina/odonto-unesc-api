aa2914e8932d424257dfbc4ea800d43682485efe
wilianx7 <wilianx7@hotmail.com>
2019-11-27 21:00:43 -0300

    Add migration

---
+++ database/migrations/20191127235815-refactor-add-field-position-case_video-table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Cria novos campos title, description, archive, active, shared e teacher
+    */
+    /*
+      Remove campos
+    */
+    const queue = [
+      queryInterface.addColumn('Video_Case', 'position', {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      })
+    ];
+
+    return Promise.all(queue);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Remove campos
+    */
+    const queue = [
+      queryInterface.removeColumn('Video_Case', 'position')
+    ];
+
+    return Promise.all(queue);
+  }
+};
